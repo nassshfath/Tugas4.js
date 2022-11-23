0bb4720c6b355ba1ddd4e8c480c76aad58d02b42
Gendrenk <nasrullohfath@gmail.com>
2022-11-23 10:10:33 +0700

    Tugas 4 JS

---
+++ Tugas4.js
@@ -0,0 +1,12 @@
+var andi = 167;
+var jaki = 172;
+var nas = 180;
+
+if(andi>jaki)
+    console.log("andi adalah siswa tertinggi")
+
+else if(jaki>nas)
+    console.log("jaki adala siswa tertinggi")
+
+else
+    console.log("nas adalah siswa tertinggi")
